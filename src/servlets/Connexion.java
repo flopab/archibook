c7651d20df250d8eb7ed4483180c1f0504e1af94
Florian Pabich <florian.pabich@yahoo.fr>
2019-07-09 23:44:58 +0200

    fix

--- src/servlets/Connexion.java
+++ src/servlets/Connexion.java
@@ -21,7 +21,7 @@ public class Connexion extends HttpServlet {
 	public static final String ATT_USER         = "utilisateur";
     public static final String ATT_FORM         = "form";
     public static final String ATT_SESSION_USER = "sessionUtilisateur";
-    public static final String VUE              = "/WEB-INF/connexion.jsp";
+    public static final String VUE              = "/connexion.jsp";
     public static final String VUE2             = "/WEB-INF/perso.jsp";
 
     public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
