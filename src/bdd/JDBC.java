26067b588a9214b1f06d7f010c8c0fef88b6d740
Florian Pabich <florian.pabich@yahoo.fr>
2019-07-09 22:23:02 +0200

    database

--- src/bdd/JDBC.java
+++ src/bdd/JDBC.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,21 +18,27 @@ public class JDBC {
     
     public boolean connection(String login, String password2) {
     	try {
-            Class.forName( "com.mysql.cj.jdbc.Driver" );
+    		Class.forName("org.postgresql.Driver");
         } catch ( ClassNotFoundException e ) {e.getMessage();
         }
     	
     	/* Connexion à la base de données */
-        String url = "jdbc:mysql://localhost:3306/test?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC";
+        String url = "jdbc:postgresql://" + "ec2-54-246-84-100.eu-west-1.compute.amazonaws.com:5432"
+				+ "/db73homml5244";
+        String utilisateur = "soaasntkjfxzfs";
+        String motDePasse = "52a39abb82302f78f5eed153c0e2c40a78906647120b41522e0ddce4c92a7270";
+        
+        
+        /*String url = "jdbc:mysql://localhost:3306/test?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC";
         String utilisateur = "java";
-        String motDePasse = "flopab";
+        String motDePasse = "flopab";*/
         Connection connexion = null;
         ResultSet resultat = null;
         try {
             connexion = DriverManager.getConnection( url, utilisateur, motDePasse );
             
             /* Requete preparee */
-            PreparedStatement preparedStatement = connexion.prepareStatement("SELECT password FROM User WHERE login = ?");
+            PreparedStatement preparedStatement = connexion.prepareStatement("SELECT password FROM members WHERE login = ?");
             preparedStatement.setString(1, login);
             resultat = preparedStatement.executeQuery();
             
@@ -58,9 +63,10 @@ public class JDBC {
         }
 
         /* Connexion à la base de données */
-        String url = "jdbc:mysql://localhost:3306/test?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC";
-        String utilisateur = "java";
-        String motDePasse = "flopab";
+        String url = "jdbc:postgresql://" + "ec2-54-246-84-100.eu-west-1.compute.amazonaws.com:5432"
+				+ "/db73homml5244";
+        String utilisateur = "soaasntkjfxzfs";
+        String motDePasse = "52a39abb82302f78f5eed153c0e2c40a78906647120b41522e0ddce4c92a7270";
         Connection connexion = null;
         Statement statement = null;
         ResultSet resultat = null;
@@ -71,7 +77,7 @@ public class JDBC {
             statement = connexion.createStatement();
 
             /* Exécution d'une requête de lecture */
-            resultat = statement.executeQuery( "SELECT commonName, surName, school FROM User;" );
+            resultat = statement.executeQuery( "SELECT commonName, surName, school FROM members;" );
      
             /* Récupération des données du résultat de la requête de lecture */
             while ( resultat.next() ) {
